5fe0f94e734c974d05273781172c465c9bf2b7fe
Shungo-I <iririn199810710@gmail.com>
2026-04-25 13:36:54 +0900

    Add setup instructions and CLI banner functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export {};
+#!/usr/bin/env node
+
+/** @see Plan.md 起動バナー案 */
+function printBanner(): void {
+  const line3 = " / /   / / __ \\/ / | / / / / / / __ `__ \\";
+
+  const lines = [
+    "",
+    "   ________      _       _",
+    "  / ____/ /___  (_)   __(_)_  ______ ___",
+    line3,
+    "/ /___/ / /_/ / /| |/ / / /_/ / / / / / /",
+    "\\____/_/\\____/_/ |___/_/\\__,_/_/ /_/ /_/",
+    "",
+    "CLI agents, gathered.",
+    "",
+  ];
+  console.log(lines.join("\n"));
+}
+
+printBanner();
